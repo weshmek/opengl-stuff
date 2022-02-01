45baae631aa174d9796e1a335cb069fd0ffce018
Wesley Chalmers <wwchalme@uwaterloo.ca>
2022-01-31 23:06:19 -0500

    Reformat shader compiler

--- shared/shader_compiler.h
+++ shared/shader_compiler.h
@@ -14,16 +14,25 @@ struct shader_program {
 
 };
 /*
- * Format string:
- * %[n](fvg)
+ * Format string
+ * {%[n]v|g|te|tc|f[nn]}+
  * n = number of source files comprising shader
  * %f = fragment shader
+ * 	fragment shader is followed by nn (int, const char*) pairs
+ * 	referring to colour attachment binding and variable name.
  * %v = vertex shader
- * %g = geometry shader
- * %c = compute shader
- * %te = tesselation evaluation shader
+ * %d = geometry shader
  * %tc = tesselation control shader
+ * %te = tesselation geometry shader
+ * return value = the program handle
+ * if compilation fails, the program handle is 0
+ *
+ * Examples
+ * 	GLuint program = compile_shaders("%v%f", "assets/vertex_shader.vs", "assets/fragment_shader.fs", "out_colour");
+ * 	GLuint program = compile_shaders("%2v%3f%2g", "assets/universal_header.sf", "assets/vertex_shader.vs", "assets/universal_header.sf", "assets/fragment_shader_1.fs", "assets/fragment_shader_2.fs", "out_colour", "assets/universal_header.sf", "assets/geometry_shader.gs");
+ * 	GLuint program = compile_shaders("%f2", "assets/fragment_shader.fs", 0, "colour1", 1, "colour2")
  */
+
 struct shader_program compile_shaders(const char *fmt, ...);
 
 #endif
